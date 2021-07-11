de5b7de4f62a55fe981136e8f6edc25537e40d98
Astroalex <alexis31.balthazard@gmail.com>
2021-07-11 20:03:29 +0200

    refactor: Change render of settings panel

--- src/renderer/components/Settings.vue
+++ src/renderer/components/Settings.vue
@@ -1,34 +1,30 @@
 <template>
-  <article class="container">
-    <n-card>
-      <n-tabs display-directive="show" type="bar" size="large" justify-content="space-evenly" style="height: 100%">
-        <n-tab-pane name="general" tab="Générales">
-          <n-result
-            status="warning"
-            title="Options générales"
-            description="Cette section est encore en construction, revenez plus tard~~"
-            style="margin-top: 5em"
-          />
-        </n-tab-pane>
-        <n-tab-pane display-directive="show" name="look" tab="Apparence">
-          <n-result
-            status="warning"
-            title="Options d'apparence"
-            description="Cette section est encore en construction, revenez plus tard~~"
-            style="margin-top: 5em"
-          />
-        </n-tab-pane>
-        <n-tab-pane display-directive="show" name="about" tab="A propos" style="text-align: center; height: 100%">
-          <about />
-        </n-tab-pane>
-      </n-tabs>
-    </n-card>
-  </article>
+  <n-tabs display-directive="if" size="large" justify-content="space-evenly" type="line">
+    <n-tab-pane name="general" tab="Générales">
+      <n-result
+        status="warning"
+        title="Options générales"
+        description="Cette section est encore en construction, revenez plus tard~~"
+        style="margin-top: 5em"
+      />
+    </n-tab-pane>
+    <n-tab-pane name="look" tab="Apparence" style="padding: 0;">
+      <look-settings />
+    </n-tab-pane>
+    <n-tab-pane name="about" tab="A propos" style="text-align: center;">
+      <about />
+    </n-tab-pane>
+  </n-tabs>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { NTabs, NTabPane, NCard, NResult } from 'naive-ui'
+import {
+  NTabs,
+  NTabPane,
+  NResult
+} from 'naive-ui'
+import LookSettings from './LookSettings.vue'
 import About from './About.vue'
 
 export default defineComponent({
@@ -36,7 +32,7 @@ export default defineComponent({
     NResult,
     NTabs,
     NTabPane,
-    NCard,
+    LookSettings,
     About
   }
 })
